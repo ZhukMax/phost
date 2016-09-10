a4bac144fe2149695bf8a0926ea79f87ff3b70ce
Max Zhuk <zhukmax@ya.ru>
2016-09-10 20:54:19 +0300

    Update phost.sh

--- phost.sh
+++ phost.sh
@@ -75,23 +75,17 @@ pm.max_spare_servers = 4
 service nginx restart
 service php7.0-fpm restart
 
-I=`dpkg -s mysql | grep "Status" `
-if [ -n "$I" ]
-then
+echo "Enter MySQL root password:"
+read ROOTPASS
 
-	echo "Enter MySQL root password:"
-	read ROOTPASS
-	
-	echo "Enter password for new DB:"
-	read MYSQLPASS
-	
-	echo "Creating database"
-	
-	Q1="CREATE DATABASE IF NOT EXISTS $USERNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;;"
-	Q2="GRANT ALTER,DELETE,DROP,CREATE,INDEX,INSERT,SELECT,UPDATE,CREATE TEMPORARY TABLES,LOCK TABLES ON $USERNAME.* TO '$USERNAME'@'localhost' IDENTIFIED BY '$MYSQLPASS';"
-	Q3="FLUSH PRIVILEGES;"
-	SQL="${Q1}${Q2}${Q3}"
-	
-	mysql -uroot --password=$ROOTPASS -e "$SQL"
+echo "Enter password for new DB:"
+read MYSQLPASS
 
-fi
+echo "Creating database"
+
+Q1="CREATE DATABASE IF NOT EXISTS $USERNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;;"
+Q2="GRANT ALTER,DELETE,DROP,CREATE,INDEX,INSERT,SELECT,UPDATE,CREATE TEMPORARY TABLES,LOCK TABLES ON $USERNAME.* TO '$USERNAME'@'localhost' IDENTIFIED BY '$MYSQLPASS';"
+Q3="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}"
+	
+mysql -uroot --password=$ROOTPASS -e "$SQL"
